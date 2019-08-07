4afcafc284f42b01b6ea66047fab3621ad3ae65d
Himanshu Soni <himanshusoni.me@gmail.com>
2019-08-07 13:46:06 +0530

    beta3 updated

--- touchconfirmview/src/main/java/me/himanshusoni/touchconvirmview/TouchConfirmView.kt
+++ touchconfirmview/src/main/java/me/himanshusoni/touchconvirmview/TouchConfirmView.kt
@@ -1,24 +1,27 @@
 package me.himanshusoni.touchconvirmview
 
+import android.annotation.TargetApi
 import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.util.AttributeSet
 import android.widget.LinearLayout
 import android.widget.TextView
+import me.himanshusoni.basicextensions.dpToPx
 
 class TouchConfirmView : LinearLayout {
 
     private var mText: String = ""
-    private var mTextAppearance: Int = 0
     private var mTextAllCaps: Boolean = true
     private var mTextSize: Int = 0
-    private var mTextColor: Int = 0
-    private var mConfirmedTextColor: Int = 0
-    private var mTextBackground: Int = 0
-    private var mConfirmedTextBackground: Int = 0
+    private var mTextColorNormal: Int = 0
+    private var mTextColorConfirmed: Int = 0
+    private var mTextBackgroundNormal: Drawable? = null
+    private var mTextBackgroundConfirmed: Drawable? = null
     private var mTextPadding: Int = 0
-    private var mTextDividerWidth: Int = 0
+    private var mTextMargin: Int = 0
 
     private var mOnTextConfirmedListener: (() -> Unit)? = null
 
@@ -34,6 +37,7 @@ class TouchConfirmView : LinearLayout {
         init(attrs, defStyle)
     }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     constructor(context: Context, attrs: AttributeSet, defStyle: Int, defStyleRes: Int) : super(
         context,
         attrs,
@@ -44,13 +48,30 @@ class TouchConfirmView : LinearLayout {
     }
 
     private fun init(attrs: AttributeSet? = null, defStyle: Int = 0, defStyleRes: Int = 0) {
-        setPadding(4.dpToPx().toInt(), 4.dpToPx().toInt(), 4.dpToPx().toInt(), 4.dpToPx().toInt())
-
         val a = context.obtainStyledAttributes(attrs, R.styleable.TouchConfirmView, defStyle, defStyleRes)
 
-        mTextAppearance = a.getResourceId(
-            R.styleable.TouchConfirmView_textAppearance,
-            R.style.TextAppearance_TouchConfirmView
+        mTextBackgroundConfirmed = a.getDrawable(R.styleable.TouchConfirmView_textBackgroundConfirmed)
+
+        mTextBackgroundNormal = a.getDrawable(R.styleable.TouchConfirmView_textBackgroundNormal)
+
+        mTextMargin = a.getDimension(
+            R.styleable.TouchConfirmView_textMargin,
+            4.dpToPx().toFloat()
+        ).toInt()
+
+        mTextPadding = a.getDimension(
+            R.styleable.TouchConfirmView_textPadding,
+            4.dpToPx().toFloat()
+        ).toInt()
+
+        mTextColorConfirmed = a.getColor(
+            R.styleable.TouchConfirmView_textColorConfirmed,
+            Color.RED
+        )
+
+        mTextColorNormal = a.getColor(
+            R.styleable.TouchConfirmView_textColorNormal,
+            Color.BLACK
         )
 
         mTextAllCaps = a.getBoolean(
@@ -63,14 +84,10 @@ class TouchConfirmView : LinearLayout {
             18.dpToPx().toInt()
         )
 
-        mTextColor = a.getColor(
-            R.styleable.TouchConfirmView_textColor,
-            android.R.attr.textColorPrimary
-        )
-
         val text = a.getString(R.styleable.TouchConfirmView_text) ?: ""
         setText(text)
 
+        // setPadding(4.dpToPx().toInt(), 4.dpToPx().toInt(), 4.dpToPx().toInt(), 4.dpToPx().toInt())
         a.recycle()
     }
 
@@ -86,26 +103,38 @@ class TouchConfirmView : LinearLayout {
         val tv = TextView(context)
 
         val lp = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        lp.setMargins(4.dpToPx().toInt(), 4.dpToPx().toInt(), 4.dpToPx().toInt(), 4.dpToPx().toInt())
+        lp.setMargins(mTextMargin, mTextMargin, mTextMargin, mTextMargin)
         tv.layoutParams = lp
 
-        tv.setPadding(16.dpToPx().toInt(), 8.dpToPx().toInt(), 16.dpToPx().toInt(), 8.dpToPx().toInt())
+        tv.setPadding(mTextPadding, mTextPadding, mTextPadding, mTextPadding)
 
         val states = arrayOf(
             intArrayOf(-android.R.attr.state_selected), // not selected
             intArrayOf(android.R.attr.state_selected)  // selected
         )
-        val colors = intArrayOf(Color.BLACK, Color.RED)
+        val colors = intArrayOf(mTextColorNormal, mTextColorConfirmed)
         tv.setTextColor(ColorStateList(states, colors))
 
-        tv.setBackgroundResource(R.drawable.tcv_bg)
-        tv.text = c.toString()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            tv.background = mTextBackgroundNormal
+        } else {
+            tv.setBackgroundDrawable(mTextBackgroundNormal)
+        }
 
-        tv.textSize = 24.dpToPx().toFloat()
-        tv.minWidth = tv.measuredHeight
+        tv.text = if (mTextAllCaps) c.toString().toUpperCase() else c.toString()
+
+        tv.textSize = mTextSize.toFloat()
 
         tv.setOnClickListener {
             tv.isSelected = !tv.isSelected
+
+            val back = if (tv.isSelected) mTextBackgroundConfirmed else mTextBackgroundNormal
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                tv.background = back
+            } else {
+                tv.setBackgroundDrawable(back)
+            }
+
             if (isTouchConfirmed()) {
                 mOnTextConfirmedListener?.invoke()
             }
--- touchconvirmview/src/main/java/me/himanshusoni/touchconvirmview/Utils.kt
+++
@@ -1,9 +0,0 @@
-package me.himanshusoni.touchconvirmview
-
-import android.content.res.Resources
-import android.util.TypedValue
-
-fun Number.toPixels(unit: Int): Number =
-    TypedValue.applyDimension(unit, this.toFloat(), Resources.getSystem().displayMetrics)
-
-fun Number.dpToPx(): Number = this.toPixels(TypedValue.COMPLEX_UNIT_DIP)
